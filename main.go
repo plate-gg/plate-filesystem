95ec1afdf90c8e369daa0de4522797c6ff497068
Ayush Kumar <ayushk2020@gmail.com>
2023-04-22 21:58:50 -0400

    Cleaning

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"strconv"
 
 	"github.com/joho/godotenv"
@@ -16,13 +15,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-
 	// Create Echo instance
 	e := echo.New()
 
 	// Routes
 	e.PUT("/api/v1/files", putFile)
-	e.GET("/api/v1/cid/*", getCid)
+	e.POST("/api/v1/cid", getCid)
 	e.POST("/api/v1/move", moveFile)
 	e.DELETE("/api/v1/delete", deleteFile)
 	e.POST("/api/v1/files", list)
@@ -35,25 +33,25 @@ func main() {
 	log.Fatal(e.Start(":" + port))
 }
 
-
-/* Request: PUT /api/v1/putFile
-		{
-			"fileName": "test.txt",
-			"path": "/test.txt", // Default is 
-			"cid": "QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ" // CID of the file
-			"size": "123456" // Size of the file in bytes
-		}
-	Return
-		{
-			"id": "5f9e1b9b9c9c2b0007e1b5f1",
-			"fileName": "test.txt",
-			"path": "/test.txt", // Default is
-		}
+/*
+	 Request: PUT /api/v1/putFile
+			{
+				"fileName": "test.txt",
+				"path": "/test.txt", // Default is
+				"cid": "QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ" // CID of the file
+				"size": "123456" // Size of the file in bytes
+			}
+		Return
+			{
+				"id": "5f9e1b9b9c9c2b0007e1b5f1",
+				"fileName": "test.txt",
+				"path": "/test.txt", // Default is
+			}
 */
 func putFile(c echo.Context) error {
 	db := Connect()
 	// convert size to int64
-	size,e := strconv.ParseInt(c.FormValue("size"), 10, 64)
+	size, e := strconv.ParseInt(c.FormValue("size"), 10, 64)
 	if e != nil {
 		log.Println(e)
 		return c.JSON(http.StatusBadRequest, e.Error())
@@ -63,7 +61,7 @@ func putFile(c echo.Context) error {
 		Path:     c.FormValue("path"),
 		FileName: c.FormValue("fileName"),
 		CID:      c.FormValue("cid"),
-		Size:    size,
+		Size:     size,
 	}
 
 	err := db.Create(&newFile).Error
@@ -75,24 +73,25 @@ func putFile(c echo.Context) error {
 	return c.JSON(http.StatusCreated, newFile)
 }
 
-/* Request: POST /api/v1/move
-		{
-			"sourcePath": "/test.txt",
-			"destinationPath": "/test2.txt"
-		}
-	Return
-		{
-			"id": "5f9e1b9b9c9c2b0007e1b5f1",
-			"fileName": "test.txt",
-			"path": "/test.txt", // Default is
-		}
+/*
+	 Request: POST /api/v1/move
+			{
+				"sourcePath": "/test.txt",
+				"destinationPath": "/test2.txt"
+			}
+		Return
+			{
+				"id": "5f9e1b9b9c9c2b0007e1b5f1",
+				"fileName": "test.txt",
+				"path": "/test.txt", // Default is
+			}
 */
 func moveFile(c echo.Context) error {
 	db := Connect()
 
 	sourcePath := c.FormValue("sourcePath")
 	destinationPath := c.FormValue("destinationPath")
-
+	
 	var file File
 	if err := db.Where("path = ?", sourcePath).First(&file).Error; err != nil {
 		log.Println(err)
@@ -108,16 +107,17 @@ func moveFile(c echo.Context) error {
 	return c.JSON(http.StatusCreated, "File moved successfully")
 }
 
-/* Request: DELETE /api/v1/file
-		{
-			"path": "/test.txt"
-		}
-	Return
-		{
-			"id": "5f9e1b9b9c9c2b0007e1b5f1",
-			"fileName": "test.txt",
-			"path": "/test.txt", // Default is
-		}
+/*
+	 Request: DELETE /api/v1/file
+			{
+				"path": "/test.txt"
+			}
+		Return
+			{
+				"id": "5f9e1b9b9c9c2b0007e1b5f1",
+				"fileName": "test.txt",
+				"path": "/test.txt", // Default is
+			}
 */
 func deleteFile(c echo.Context) error {
 	db := Connect()
@@ -132,44 +132,42 @@ func deleteFile(c echo.Context) error {
 	return c.JSON(http.StatusCreated, "File deleted successfully")
 }
 
-/* Request: POST /api/v1/list
-		{
-			"path": "/test.txt"
-		}
-	Return
-		{
-			[
-				{
-			"id": "5f9e1b9b9c9c2b0007e1b5f1",
-			"fileName": "test.txt",
-			"path": "/test.txt", // Default is
+/*
+	 Request: POST /api/v1/list
 			{
-			"id": "5f9e1b9b9c9c2b0007e1asd1",
-			"fileName": "tes2t.txt",
-			"path": "/test2.txt", // Default is
-			]
-		}
+				"path": "/test.txt"
+			}
+		Return
+			{
+				[
+					{
+				"id": "5f9e1b9b9c9c2b0007e1b5f1",
+				"fileName": "test.txt",
+				"path": "/test.txt", // Default is
+				{
+				"id": "5f9e1b9b9c9c2b0007e1asd1",
+				"fileName": "tes2t.txt",
+				"path": "/test2.txt", // Default is
+				]
+			}
 */
 func list(c echo.Context) error {
 	db := Connect()
 
 	path := c.FormValue("path")
 
-	//A regular expression to match subpaths with one more level
-	regex := "^" + regexp.QuoteMeta(path) + "/[^/]+$"
-
-	// Find all files with path matching the regex and return their paths in an array 
+	// Find all files with path matching the regex and return their paths in an array
 	var files []File
-	if err := db.Where("path ~ ?", regex).Find(&files).Error; err != nil {
+
+	if err := db.Raw("SELECT * FROM files WHERE path LIKE ? || '%' AND deleted_at IS NULL", path).Scan(&files).Error; err != nil {
 		log.Println(err)
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+
 	var paths []string
 	for _, file := range files {
 		paths = append(paths, file.Path)
 	}
-	
-	
 
 	return c.JSON(http.StatusOK, files)
 }
@@ -177,7 +175,7 @@ func list(c echo.Context) error {
 func getCid(c echo.Context) error {
 	db := Connect()
 
-	path := c.Param("*")
+	path := c.FormValue("path")
 
 	var file File
 	if err := db.Where("path = ?", path).First(&file).Error; err != nil {
